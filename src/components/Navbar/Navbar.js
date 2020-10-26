c4181ba8917fb41c8fa99c451df72fe9b8d569f2
Lindsey Smith <campbelllm@gmail.com>
2020-10-26 09:57:46 -0600

    navbar styling done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,23 @@
 import React from "react";
-import { Logo, Menu, StyledLink } from "./navbarStyledComponents";
-import "./navbar.css";
+import { Logo, Nav, Menu, StyledLink } from "./navbarStyledComponents";
+// import "./navbar.css";
 import logo from "../../Images/logo.png";
 
 const Navbar = () => {
   return (
-    <nav>
+    <Nav>
       <Logo>
         {" "}
         <img src={logo} alt="logo" />
-        Lindsey Smith
+        <p>Lindsey Smith</p>
       </Logo>
       <Menu>
         <StyledLink to="/Home">Home</StyledLink>
         <StyledLink to="/about">About</StyledLink>
         <StyledLink to="/contact">Contact</StyledLink>
         <StyledLink to="/portfolio">Portfolio</StyledLink>
-      </Menu>
-    </nav>
+        </Menu>
+    </Nav>
   );
 };
 
--- src/components/Navbar/navbarStyledComponents.js
+++ src/components/Navbar/navbarStyledComponents.js
@@ -2,29 +2,42 @@ import styled from "styled-components/macro";
 import { NavLink } from "react-router-dom";
 import Flexbox from "flexbox-react";
 
-export const Navbar = styled.nav`
+export const Nav = styled.nav`
     background-color: #F2E8CF;
     color: #295151;
     border-width: thin;
     border-bottom-style: solid;
     border-bottom-color: #295151;
     position: fixed;
+    width: 100%;
+    font-size: 18px;
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `;
 
 export const Logo = styled.div`
-  display: inline-block;
+  display: flex;
+
   & img{
     width: 30px;
+  }
+
+  & p{
+    margin: 0;
+    padding: 5px 10px;
+    font-size: 20px;
   }
 `;
 
 export const Menu = styled.div`
-  float: right;
+  
 `;
 
 export const StyledLink = styled(NavLink)`
   color: #295151;
-  margin-right: 20px;
+  margin: 20px;
   text-decoration: none;
 
 
