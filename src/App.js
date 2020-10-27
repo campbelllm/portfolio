16f715ea3f1befdfff689c39b326b051a2c056d1
Lindsey Smith <campbelllm@gmail.com>
2020-10-27 13:52:50 -0600

    start of contact component, added all routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './components/Footer/Footer'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
-
+import Contact from './components/Contact/Contact'
 
 function App() {
   return (
@@ -12,6 +12,8 @@ function App() {
       <Navbar />
       <Route exact path='/' component={Home} />
       <Route path='/about' component={About} />
+      <Route path='/contact' component={Contact} />
+      <Route path='/portfolio' component={Portfolio} />
       <Footer />
     </Router>
   );
