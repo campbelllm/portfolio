afcefe23c2335532956c0658ff90920105656ee9
Lindsey Smith <campbelllm@gmail.com>
2020-10-27 14:56:49 -0600

    button styling done, some input styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,33 +1,66 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { ContactSection, ContactForm, NameAndEmail, Subject, Message }from './contactStyledComponents'
-
+import React from "react";
+import { useForm } from "react-hook-form";
+import {
+  ContactSection,
+  ContactForm,
+  NameAndEmail,
+  Subject,
+  Message,
+  Email,
+  Name,
+  StyledButton,
+} from "./contactStyledComponents";
 
 const Contact = () => {
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => console.log(data);
+  const onSubmit = (data) => console.log(data);
   console.log(errors);
   return (
     <ContactSection>
       <ContactForm>
         <h1>Contact Me</h1>
-        <p>I would love to hear from you! Send me feedback or ask me anything.</p>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <NameAndEmail>
-      <input type="text" placeholder="Your Name" name="Name" ref={register({required: true, maxLength: 80})} />
-      <input type="text" placeholder="Email" name="Email" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
-      </NameAndEmail>
-      <Subject>
-      <input type="text" placeholder="Subject" name="Subject" ref={register({required: true,  maxLength: 200})} />
-      </Subject>
-      <Message>
-      <input type="text" placeholder="Message" name="Message" ref={register({required: true,  maxLength: 1000})} />
-      </Message>
-
-      <button type="submit">Submit</button>
-    </form>
-    </ContactForm>
+        <p>
+          I would love to hear from you! Send me feedback or ask me anything.
+        </p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <NameAndEmail>
+            <Name>
+              <input
+                type="text"
+                name="name"
+                ref={register({ required: true, maxLength: 80 })}
+              />
+              <label for="name">Name</label>
+            </Name>
+            <Email>
+              <input
+                type="text"
+                name="email"
+                ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+              />
+              <label for="email">Email</label>
+            </Email>
+          </NameAndEmail>
+          <Subject>
+            <input
+              type="text"
+              name="subject"
+              ref={register({ required: true, maxLength: 200 })}
+            />
+            <label for="subject">Subject</label>
+          </Subject>
+          <Message>
+            <input
+              type="text"
+              name="message"
+              ref={register({ required: true, maxLength: 1000 })}
+            />
+            <label for="Message">Message</label>
+          </Message>
+          <StyledButton type="submit">Submit</StyledButton>
+        </form>
+      </ContactForm>
     </ContactSection>
   );
-}
- export default Contact;
+};
+export default Contact;
--- src/components/Contact/contactStyledComponents.js
+++ src/components/Contact/contactStyledComponents.js
@@ -10,26 +10,52 @@ export const ContactForm = styled.div`
   background-color: #819c8b;
   width: 90%;
   color: #f2e8cf;
-  padding: 200px;
+  padding: 100px;
 
-  & input{
-  border:none;
-     border-bottom: 2px solid #295151;
-     background-color: #819C8B;
-     border-radius: 0;
-}
-  
+  & input {
+    border: none;
+    border-bottom: 2px solid #295151;
+    background-color: #819c8b;
+    border-radius: 0;
+  }
 `;
 
 export const NameAndEmail = styled.div`
+  & input {
+    font-size: 16px;
+  }
+`;
 
-   
+export const Name = styled.div`
+  margin-right: 5px;
+  display: inline-grid;
+  & label {
+    float: left;
+  }
+`;
+
+export const Email = styled.div`
+  display: inline-grid;
 `;
 
 export const Subject = styled.div`
-
+  display: inline-grid;
 `;
 
 export const Message = styled.div`
+  display: inline-grid;
+`;
 
+export const StyledButton = styled.div`
+  background-color: #295151;
+  border: none;
+  padding: 10px 20px;
+  margin-top: 20px;
+  width: 100px;
+  border-radius: 4px;
+  text-align: center;
+
+  &:hover {
+    background-color: #805858;
+  }
 `;
