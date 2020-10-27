e1cf269d52f221e7d93ea4f094211428967983e5
Lindsey Smith <campbelllm@gmail.com>
2020-10-27 14:31:29 -0600

    all components for contact page done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { ContactSection, ContactForm, NameAndEmail, Subject, Message }from './contactStyledComponents'
+
 
 const Contact = () => {
-  return{
-    
-  }
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+  return (
+    <ContactSection>
+      <ContactForm>
+        <h1>Contact Me</h1>
+        <p>I would love to hear from you! Send me feedback or ask me anything.</p>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <NameAndEmail>
+      <input type="text" placeholder="Your Name" name="Name" ref={register({required: true, maxLength: 80})} />
+      <input type="text" placeholder="Email" name="Email" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
+      </NameAndEmail>
+      <Subject>
+      <input type="text" placeholder="Subject" name="Subject" ref={register({required: true,  maxLength: 200})} />
+      </Subject>
+      <Message>
+      <input type="text" placeholder="Message" name="Message" ref={register({required: true,  maxLength: 1000})} />
+      </Message>
+
+      <button type="submit">Submit</button>
+    </form>
+    </ContactForm>
+    </ContactSection>
+  );
 }
+ export default Contact;
--- src/components/Contact/contactStyledComponents.js
+++ src/components/Contact/contactStyledComponents.js
@@ -1,2 +1,35 @@
-import styled from 'styled-components/macro'
+import styled from "styled-components/macro";
 
+export const ContactSection = styled.div`
+  padding: 200px 0px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ContactForm = styled.div`
+  background-color: #819c8b;
+  width: 90%;
+  color: #f2e8cf;
+  padding: 200px;
+
+  & input{
+  border:none;
+     border-bottom: 2px solid #295151;
+     background-color: #819C8B;
+     border-radius: 0;
+}
+  
+`;
+
+export const NameAndEmail = styled.div`
+
+   
+`;
+
+export const Subject = styled.div`
+
+`;
+
+export const Message = styled.div`
+
+`;
