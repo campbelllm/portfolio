24eb45fd40f08d03ff39e1a767c83878fc50f3ab
Lindsey Smith <campbelllm@gmail.com>
2023-01-11 09:56:49 -0800

    styling transition for contact link

--- src/components/Header/headerStyledComponents.js
+++ src/components/Header/headerStyledComponents.js
@@ -34,10 +34,43 @@ export const Contact = styled.div`
     & a {
         color: black; 
         text-decoration: none;
+        padding: 10px 20px ;
     }
-    & a:hover{
-        color: pink;
+    & a:before{
+        position: absolute;
+        content: "";
+        height: 0;
+        width: 0;
+        border: 1px solid transparent;
+        box-sizing: border-box;
+       
+    }
+    & a:hover:before{
+        color: black;
         text-decoration: none; 
+        border: 1px solid black;
+        transition: height 0.5s linear,
+                    width 0.5s linear 0.5s;
+        border-right: none;
+        border-bottom: none;
+    }
+    & a:after{
+        position: absolute;
+        content: "";
+        height: 0;
+        width: 0;
+        border: 1px solid transparent;
+        box-sizing: border-box;
+       
+    }
+    & a:hover:after{
+        color: black;
+        text-decoration: none; 
+        border: 1px solid black;
+        transition: height 0.5s linear,
+                    width 0.5s linear 0.5s;
+        border-left: none;
+        border-top: none;
     }
 
 `
