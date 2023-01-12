df910580bb41b75c6f1f9d35f4af43f7a37bbdae
Lindsey Smith <campbelllm@gmail.com>
2023-01-11 20:47:31 -0800

    add Projects component

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {ProjectSection} from "./projectsStyledComponent"
+
+const Projects = () => {
+    return(
+        <ProjectSection/>
+    )
+    
+}
+
+export default Projects;
