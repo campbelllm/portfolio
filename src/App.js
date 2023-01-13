9b0dfdd3680b556d8ca0c4cceecbc74111d0d3b4
Lindsey Smith <campbelllm@gmail.com>
2023-01-13 12:48:54 -0800

    updated react router'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-// import Navbar from './components/Navbar/Navbar';
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Contact from "./components/Contact/Contact";
