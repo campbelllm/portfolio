c214ad8f7fd4aeaae700e6b8242d9ede8e711972
Lindsey Smith <campbelllm@gmail.com>
2023-01-13 11:57:01 -0800

    add switch to app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 // import Navbar from './components/Navbar/Navbar';
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
     <Router className="body">
-      <Route exact path="/" component={Home} />
-      <Route exact path="/contact" component={Contact} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
     </Router>
   );
 }
