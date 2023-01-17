b791e002fa59b223e9fd5125e79da210698bd53c
Lindsey Smith <campbelllm@gmail.com>
2023-01-17 14:39:35 -0800

    submit format change

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -46,7 +46,6 @@ const Contact = () => {
                 type="email"
                 name="email"
                 placeholder="where can I reach you?"
-                pattern="/^\S+@\S+$/i"
                 required
               />
             </Email>
@@ -72,57 +71,16 @@ const Contact = () => {
             ></textarea>
           </Message>
           <StyledButton>
-            <input type="submit" name="submit">
-              SEND <i class="fa fa-send-o"></i>
-            </input>
+          
+            <input
+              type="submit"
+              name="submit"
+              value="SEND"
+            />
           </StyledButton>
         </form>
       </ContactForm>
     </ContactSection>
-    // <form  name="contact" method="post"  >
-    // <input type="hidden" name="form-name" value="contact"/>
-
-    //       <label type="name">name:</label>
-    //       <input
-    //         type="text"
-    //         name="name"
-    //         placeholder="who are you?"
-    //         maxLength= "200"
-    //         required
-    //       />
-
-    //       <label type="email">email:</label>
-    //       <input
-    //         type="email"
-    //         name="email"
-    //         placeholder="where can I reach you?"
-    //         required
-    //       />
-
-    //     <label type="subject">subject:</label>
-    //     <input
-    //       type="text"
-    //       name="subject"
-    //       placeholder="what are we chatting about?"
-    //       maxLength= "200"
-    //       required
-    //     />
-
-    //     <label type="Message">message:</label>
-    //     <textarea
-    //       type="text"
-    //       name="message"
-    //       placeholder="tell me more..."
-    //       maxLength= "5000"
-    //       required
-    //     ></textarea>
-
-    //     <input type="submit"
-    //     name="submit"
-    //     value="send"
-    //   />
-
-    // </form>
   );
 };
 
